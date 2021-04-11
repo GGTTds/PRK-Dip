bb56565789fc5bc3bf349fc69cb93c2014f13426
tpnotllnptl@gmail.com <tpnotllnptl@gmail.com>
2021-04-11 16:52:25 +0500

    #WE-7 Добавление\Редактирование данных в таблице Tich => ychi.cs

---
+++ SADIC/ADDTich.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SADIC
+{
+    /// <summary>
+    /// Логика взаимодействия для ADDTich.xaml
+    /// </summary>
+    public partial class ADDTich : Window
+    {
+        private Tich _currentPost = new Tich();
+        private int num = 0;
+        public ADDTich(Tich Se2Wq, int k)
+        {
+            InitializeComponent();
+            gr.ItemsSource = EWnter.Qwer().Group.ToList();
+            mer.ItemsSource = EWnter.Qwer().Event.ToList();
+            za.ItemsSource = EWnter.Qwer().Zanztia.ToList();
+            re.ItemsSource = EWnter.Qwer().Roul.ToList();
+            if (Se2Wq != null)
+            {
+                _currentPost = Se2Wq;
+                num = k;
+            }
+            DataContext = _currentPost;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (num == 0)
+                    EWnter.Qwer().Tich.Add(_currentPost);
+                EWnter.Qwer().SaveChanges();
+                MessageBox.Show("Запись сохранена!");
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- SADIC/Window1/ychi.xaml.cs
+++ SADIC/Window1/ychi.xaml.cs
@@ -22,6 +22,62 @@ namespace SADIC
         public ychi()
         {
             InitializeComponent();
+            DataTour.ItemsSource = EWnter.Qwer().Tich.ToList();
+        }
+
+        private void toFio_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var s = EWnter.Qwer().Tich.ToList();
+            if (toFio.Text.Length != 0)
+            {
+                s = s.Where(p => p.FIOTic.ToLower().Contains(toFio.Text.ToLower())).ToList();
+               
+            }
+            else { EWnter.Qwer().Tich.ToList(); }
+            DataTour.ItemsSource = s;
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            EWnter.Qwer().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+            EWnter.Qwer().Tich.ToList();
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            ADDTich ww = new ADDTich((sender as Button).DataContext as Tich, 1);
+            ww.Show();
+        }
+
+        private void del_Click(object sender, RoutedEventArgs e)
+        {
+            var DEle =
+          DataTour.SelectedItems.Cast<Tich>().ToList();
+            var countRow = DEle.Count();
+            var res = MessageBox.Show($"Вы точно хотите удалить {countRow} записей?",
+           "Внимание!",
+            MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (res == MessageBoxResult.Yes)
+            {
+                try //команды могут выдавать ошибку, поэтому используем try catch
+                {
+                    EWnter.Qwer().Tich.RemoveRange(DEle);
+                    //БД обновляем
+                    EWnter.Qwer().SaveChanges();
+                    MessageBox.Show("Записи удалены!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ADDTich ww = new ADDTich((sender as Button).DataContext as Tich, 1);
+            ww.Show();
         }
     }
 }
